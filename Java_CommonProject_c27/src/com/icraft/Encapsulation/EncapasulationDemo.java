0d76b9d05472fe1d214c8c855115e93d56b64a11
craftgithubuser <craftknowledge.soresa@gmail.com>
2024-10-24 05:14:13 +0300

    Create EncapasulationDemo.java

---
+++ Java_CommonProject_c27/src/com/icraft/Encapsulation/EncapasulationDemo.java
@@ -0,0 +1,39 @@
+package com.icraft.Encapsulation;
+
+public class EncapasulationDemo {
+	
+	String firstName;
+	int age;
+	
+	
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
