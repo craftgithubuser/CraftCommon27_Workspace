aba83d9d8c937f0b7f8e38fd126d522aa526b33e
gman2001-1 <gizechewwolde@gmail.com>
2024-10-25 00:27:25 -0500

    Create Gdemo.java

---
+++ logical_operators/src/logical_operators/Gdemo.java
@@ -0,0 +1,11 @@
+package src.logical_operators;
+
+public class Gdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a =4;
+System.out.println(a);
+	}
+
+}
