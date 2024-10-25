b9f50c95c19948f1f47445d4ec3b2864f94eb689
gman2001-1 <gizechewwolde@gmail.com>
2024-10-25 00:35:12 -0500

    Update Gdemo.java

--- logical_operators/src/logical_operators/Gdemo.java
+++ logical_operators/src/logical_operators/Gdemo.java
@@ -1,11 +1,13 @@
-package src.logical_operators;
+package logical_operators;
 
 public class Gdemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int a =4;
+String name = "abel";
 System.out.println(a);
+System.out.println(name);
 	}
 
 }
