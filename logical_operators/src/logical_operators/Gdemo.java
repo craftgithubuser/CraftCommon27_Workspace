db3f24cc888fb7fae02b3e498e6d2f754a0a2f77
gman2001-1 <gizechewwolde@gmail.com>
2024-10-26 01:12:07 -0500

    g

--- logical_operators/src/logical_operators/Gdemo.java
+++ logical_operators/src/logical_operators/Gdemo.java
@@ -5,6 +5,7 @@ public class Gdemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int a =4;
+int b =3;
 String name = "abel";
 System.out.println(a);
 System.out.println(name);
