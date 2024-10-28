984852dba2342d01d6af0e66c9d744a14c5c94c7
Tsige-W <kibrutsige23@gmail.com>
2024-10-28 18:47:54 -0400

    My Exercise

---
+++ Revision/src/stringHandling/SecondString.java
@@ -0,0 +1,21 @@
+package stringHandling;
+
+public class SecondString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "james";
+		
+		System.out.println(name.concat(" bond"));
+		
+		
+		System.out.println(name);
+		
+		String strNew = "i "+"like "+"java";
+		
+		System.out.println(strNew);
+		
+	}
+
+}
